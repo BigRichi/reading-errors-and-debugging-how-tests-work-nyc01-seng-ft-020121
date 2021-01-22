5dfce1cec6155f85f9cb2b6997f473a5d3fff1bc
Richard Adule <big.richi@outlook.com>
2021-01-22 00:06:12 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,6 @@
 first_number = 1991
-second_number = 2021
+second_number = 2021.0
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
+quotient = first_number / second_number
